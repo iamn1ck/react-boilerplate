cd5d8691d694b7fbe8049d1f2988e81c18be703f
Akkocak-Akeski <96787871+Akkocak-Akeski@users.noreply.github.com>
2022-05-11 16:31:22 +0200

    added buttons

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,66 +1,63 @@
 import { Button, Typography } from "@mui/material";
 import { Box, styled } from "@mui/system";
-import { FC } from "react";
+import { useState, FC } from "react";
 import { useTheme } from "../context/ThemeProvider";
 import PaletteChanger from "./Layout/PaletteChanger";
 import styles from "./main.module.css";
-
-const Container = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  min-width: 500px;
-`;
-
-const ButtonsWrapper = styled(Box)`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  /* @noflip */
-  direction: ltr;
-`;
-
-const DirectionButton = styled(Button)``;
-
-const ContentWrapper = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  align-items: center;
-  margin-top: 18px;
-`;
+import axios from "axios";
 
 const Layout: FC = () => {
-  const { setDirection, direction } = useTheme();
+  const [name, setName] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [lang, setLang] = useState(1);
+
+  const buttonClickFunction = async () => {
+    console.log('you clicked the button', name)
+
+    const response = await axios.get(`https://toapi.bet3000.net/api/sports/countries/1/${lang}/300`)
+
+
+    console.log('res', response.data)
+    setCountries(response.data)
+  }
+  const changeLang = async (input: number) => {
+    setLang(input)
+  }
+
+
+
 
   return (
     <Box className={styles.box}>
       <Box className={styles.container}>
-        <ButtonsWrapper>
-          <DirectionButton
-            variant="contained"
-            color="secondary"
-            onClick={() => setDirection("ltr")}
-          >
-            LTR
-          </DirectionButton>
-          <DirectionButton
-            variant="contained"
-            onClick={() => setDirection("rtl")}
-          >
-            RTL
-          </DirectionButton>
-        </ButtonsWrapper>
-        <ButtonsWrapper mt="18px">
-          <PaletteChanger />
-        </ButtonsWrapper>
-        <ContentWrapper>
-          <Typography>
-            {direction === "rtl" ? "Right to Left" : "Left to Right"}
-          </Typography>
-          <Box mt="12px">
-            <Typography>{`----------->`}</Typography>
-          </Box>
-        </ContentWrapper>
+        <label>
+          Name:
+          <input onChange={(e) => setName(e.target.value)} type="text" name="name" />
+        </label>
+
+        <button className={styles.button} onClick={() => buttonClickFunction()}>
+
+        </button>
+        <button className={styles.button} onClick={() => changeLang(1)}>
+        english
+        </button>
+
+        <button className={styles.button} onClick={() => changeLang(6)}>
+        german
+        </button>
+
+        {name + ' is gay'}
+
+        <Box>
+          {countries.map((country: any) => {
+            return <Box>
+              {`ich hab geile huren mit dicken ärschen in ${country.CategoryName}`}
+            </Box>
+          })}
+        </Box>
+
+
+
       </Box>
     </Box>
   );
